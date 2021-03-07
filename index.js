5ef6d4d5142b0fb553bb4f556d8cb48fab20e8b7
Kartikeychandra <40001828+Kartikeychandraa@users.noreply.github.com>
2021-03-07 06:45:51 +0000

    homepage ready

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.set("view engine", "ejs");
 app.get('/',(req,res)=>{
 	res.render("home");
 })
+app.get('/login',(req,res)=>{
+	res.render("login");
+})
+app.get('/signup',(req,res)=>{
+	res.render("signup");
+})
+
 
 app.listen(process.env.PORT || 3000,()=>{
 	console.log("Server Running at 3000");
